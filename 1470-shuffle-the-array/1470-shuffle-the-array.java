618de12780c3c872894c4076b812220dc08be801
SHREYASH AWASTHI <shreyashawasthi71@gmail.com>
2026-06-15 11:47:49 +0530

    Time: 0 ms (100%), Space: 46.3 MB (79.79%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int [] arr=new int[n*2];
+        for(int i=0;i<n;i++){
+            int m=i%2;
+            arr[2*i]=nums[i];
+            arr[2*i+1]=nums[i+n];
+        }
+        return arr;
+    }
+}
