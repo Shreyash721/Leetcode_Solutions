3914c4fa927c3ce4d8faf7464b381136eee22039
SHREYASH AWASTHI <shreyashawasthi71@gmail.com>
2026-06-15 10:02:47 +0530

    Time: 4 ms (44.98%), Space: 42.4 MB (15.81%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int fib(int n) {
+        int a=0;
+        int b=1;
+        int c=0;
+       if(n==1){
+        return 1;
+       }
+       if(n==2){
+        return 1;
+       }
+       else{
+        for(int i=2;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+            System.out.print(c);
+        }
+        return c;
+       }
+    }
+}
+        
