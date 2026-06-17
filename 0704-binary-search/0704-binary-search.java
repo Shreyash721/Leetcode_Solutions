5877ab3f994a3add5bd45cb25ac5984df42d9efc
SHREYASH AWASTHI <shreyashawasthi71@gmail.com>
2026-06-17 09:19:38 +0530

    Time: 0 ms (100%), Space: 48.5 MB (13.89%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,6 +1,29 @@
 class Solution {
+    /* RECURSION METHOD */  
+
+    public int binarysearch(int[] nums,int tar,int low,int high){
+        int mid=low+(high-low)/2;
+        if(low>high) return -1;
+
+        if(nums[mid]==tar){
+            return mid;
+        }
+        else if(nums[mid]>tar){
+            return binarysearch(nums,tar,low,mid-1);
+        }
+        else return binarysearch(nums,tar,mid+1,high);
+
+    }
     public int search(int[] nums, int target) {
-        int beg=0;
+           int n=nums.length;
+        return binarysearch(nums,target,0,n-1);
+
+
+
+
+
+
+        /*int beg=0;
         int end=nums.length-1;
         while(beg<=end){
             int mid=beg+(end-beg)/2;
@@ -8,6 +31,6 @@ class Solution {
             else if(nums[mid]<target) beg=mid+1;
             else if(nums[mid]>target) end=mid-1;
         }
-        return -1;
+        return -1;*/
     }
 }
