7640be9baae5dc6f9472695b40fb0130cfbe1d96
SHREYASH AWASTHI <shreyashawasthi71@gmail.com>
2026-06-18 13:42:17 +0530

    Time: 2 ms (71.95%), Space: 45 MB (13.67%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,16 +1,16 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> str=new ArrayList<>();
-        GP(n,0,0,"",str);
+        GP(n,0,0,"",str); 
         return str;
     }
+
     public void GP(int n,int l,int r,String s,List<String> str){
-        if(r==n && l==n){
+        if(l==n && r==n){
             str.add(s);
             return;
         }
         if(l<n) GP(n,l+1,r,s+"(",str);
-
         if(r<l) GP(n,l,r+1,s+")",str);
     }
 }
