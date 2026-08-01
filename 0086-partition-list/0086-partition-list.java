eff81cf7f15dd3f9d8aeee1c41d343b74dc3eed0
SHREYASH AWASTHI <shreyashawasthi71@gmail.com>
2026-08-01 16:00:17 +0530

    Time: 0 ms (100%), Space: 43.8 MB (51.63%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -1,28 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode partition(ListNode head, int x) {
+        ListNode dummy1=new ListNode(-1);
+        ListNode dummy2=new ListNode(-1);
+        ListNode t1=dummy1;
+        ListNode t2=dummy2;
 
-        ListNode dummy1 = new ListNode(-1);
-        ListNode dummy2 = new ListNode(-1);
+        while(head!=null){
 
-        ListNode temp1 = dummy1;
-        ListNode temp2 = dummy2;
-
-        while (head != null) {
-            ListNode next = head.next;
-            head.next = null;
-
-            if (head.val < x) {
-                temp1.next = head;
-                temp1 = temp1.next;
-            } else {
-                temp2.next = head;
-                temp2 = temp2.next;
+            ListNode nextHead=head.next;
+            head.next=null;
+            if(head.val<x){
+                t1.next=head;
+                t1=t1.next;
             }
-
-            head = next;
+            else{
+                t2.next=head;
+                t2=t2.next;
+            }
+            
+            head=nextHead;
         }
 
-        temp1.next = dummy2.next;
+        ListNode i=dummy1;
+        while(i.next!=null){
+            i=i.next;
+        }
+
+        i.next=dummy2.next;
 
         return dummy1.next;
     }
