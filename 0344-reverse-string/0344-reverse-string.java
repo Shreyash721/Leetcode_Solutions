6cc8976838ed36b76ea599f5ab84e8cb2e192c18
SHREYASH AWASTHI <shreyashawasthi71@gmail.com>
2026-08-06 00:50:32 +0530

    Time: 5 ms (3.75%), Space: 50.7 MB (5.18%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public void reverseString(char[] s) {
-        int i=0;
+       /* int i=0;
         int j=s.length-1;
         while(i<=j){
             char t=s[i];
@@ -8,6 +8,23 @@ class Solution {
             s[j]=t;
             i++;
             j--;  
-        }
+        }*/
+
+        Stack<Character> st = new Stack<>();
+int i = 0;
+
+while(i != s.length){
+    st.push(s[i]);     
+    i++;
+}
+
+int tos = st.size();
+i = 0;
+
+while(tos > 0){
+    s[i] = st.pop();    
+    i++;
+    tos--;
+}
     }
 }
