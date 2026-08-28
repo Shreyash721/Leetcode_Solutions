11aac336a9a0e3e2c7a9f2f4357b4e8b288038c7
SHREYASH AWASTHI <shreyashawasthi71@gmail.com>
2026-08-28 16:34:20 +0530

    Time: 7 ms (55.11%), Space: 63.9 MB (61.8%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class quad{
+    int max;
+    int min;
+    int sum;
+    boolean isbst;
+
+    quad(int max,int min,int sum,boolean isbst){
+
+        this.max=max;
+        this.min=min;
+        this.sum=sum;
+        this.isbst=isbst;
+    }
+}
+class Solution {
+    int maxSum;;
+    public int maxSumBST(TreeNode root) {
+        maxSum=0;
+        helper(root);
+        return maxSum;
+    }
+    int sum=0;
+    quad helper(TreeNode root){
+
+        if(root==null) return new quad(Integer.MIN_VALUE,Integer.MAX_VALUE,0,true);
+
+        quad lst=helper(root.left);
+        quad rst=helper(root.right);
+
+        int max=Math.max(root.val,Math.max(lst.max,rst.max));
+        int min=Math.min(root.val,Math.min(lst.min,rst.min));
+
+        int sum=root.val+lst.sum+rst.sum;
+
+        boolean isbst=lst.isbst && rst.isbst && (lst.max<root.val && rst.min>root.val);
+
+        if(isbst) maxSum=Math.max(sum,maxSum);
+
+        return new quad(max,min,sum,isbst);
+
+    }
+}
