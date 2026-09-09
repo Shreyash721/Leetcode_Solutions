d640c631beeb54000dfbe6778ddbe17d1f8d04d9
SHREYASH AWASTHI <shreyashawasthi71@gmail.com>
2026-09-09 23:40:51 +0530

    Time: 1 ms (83.58%), Space: 42.8 MB (22.43%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -3,9 +3,6 @@ class Solution {
         int c=0;
         for(int i=1000;i<=n; i*=1000) {
             c+=n-i+1;
-            if(i>n/1000) {
-                break;
-            }
         }
         return c;
     }
